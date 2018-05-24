24a4d5132c5a433ba0662ab4c8ccdcc8c2803d79
postolenko <denis.postolenko@gmail.com>
2018-05-24 18:06:38 +0300

    Service Print

--- js/additional_scripts.js
+++ js/additional_scripts.js
@@ -46,64 +46,67 @@ $(document).ready(function() {
         speed: 1200,
         slidesToShow: 1,
         slidesToScroll: 1
-        // fade: true
     });
 
-    var range1 = document.getElementById('range_1');
-    var range2 = document.getElementById('range_2');
+    if( $("#range_1").length > 0 && $("#range_2").length > 0 ) {
 
-    noUiSlider.create(range1, {
-		start: 1350,
-		connect: true,
-		tooltips: true,
-		format: wNumb({
-	        decimals: 0
-	    }),
-		orientation: 'vertical',
-		range: {
-			'min': 0,
-			'max': 10000
-		}
-	});
+	    var range1 = document.getElementById('range_1');
+	    var range2 = document.getElementById('range_2');
 
-	 noUiSlider.create(range2, {
-		start: 0,
-		connect: true,
-		tooltips: true,
-		format: wNumb({
-	        decimals: 0
-	    }),
-		range: {
-			'min': 0,
-			'max': 10000
-		}
-	});
+	    noUiSlider.create(range1, {
+			start: 1350,
+			connect: true,
+			tooltips: true,
+			format: wNumb({
+		        decimals: 0
+		    }),
+			orientation: 'vertical',
+			range: {
+				'min': 0,
+				'max': 10000
+			}
+		});
 
-	range1.noUiSlider.on('update', function( values, handle ){
-		$("#range1_val").val(parseInt( values[handle] ) );
-	});
+		 noUiSlider.create(range2, {
+			start: 0,
+			connect: true,
+			tooltips: true,
+			format: wNumb({
+		        decimals: 0
+		    }),
+			range: {
+				'min': 0,
+				'max': 10000
+			}
+		});
 
-	range2.noUiSlider.on('update', function( values, handle ){
-		$("#range2_val").val(parseInt( values[handle] ) );
-	});
+		range1.noUiSlider.on('update', function( values, handle ){
+			$("#range1_val").val(parseInt( values[handle] ) );
+		});
 
-	// --------------------
+		range2.noUiSlider.on('update', function( values, handle ){
+			$("#range2_val").val(parseInt( values[handle] ) );
+		});
 
-	var inputRange;
+		// --------------------
 
-	var range1Val = document.getElementById('range1_val');
+		var inputRange;
 
-	range1Val.addEventListener('change', function(){
-		inputRange = parseInt( $(this).val() );
-        range1.noUiSlider.set([inputRange, null]);
-	});
+		var range1Val = document.getElementById('range1_val');
 
-	var range2Val = document.getElementById('range2_val');
+		range1Val.addEventListener('change', function(){
+			inputRange = parseInt( $(this).val() );
+	        range1.noUiSlider.set([inputRange, null]);
+		});
 
-	range2Val.addEventListener('change', function(){
-		inputRange = parseInt( $(this).val() );
-        range2.noUiSlider.set([inputRange, null]);
-	});
+		var range2Val = document.getElementById('range2_val');
+
+		range2Val.addEventListener('change', function(){
+			inputRange = parseInt( $(this).val() );
+	        range2.noUiSlider.set([inputRange, null]);
+		});
+
+	}
 
 
 });
